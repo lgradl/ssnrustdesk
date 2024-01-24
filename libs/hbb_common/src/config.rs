e1c8dd800e5b27c07534fea29099ccb81a1bb393
lgradl <96887518+lgradl@users.noreply.github.com>
2024-01-24 14:21:29 +0100

    Update config.rs
    changed to ssn settings

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -94,8 +94,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["monitoring.ssn.at"];
+pub const PUBLIC_RS_PUB_KEY: &str = "hyfZriuT+pDrAV5Y6CbzS8taXxarp+gsYW9BIo0iFIA=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
